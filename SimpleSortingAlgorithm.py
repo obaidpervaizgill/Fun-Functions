be8ada56367f69ec5c6fab892004b6f297a536d6
Obaid Pervaiz Gill <obaidpervaizgill@gmail.com>
2016-07-02 17:02:15 +1000

    Add files via upload

---
+++ SimpleSortingAlgorithm.py
@@ -0,0 +1,16 @@
+#a very very very simple sorting algorithm
+integer = [2,5,4,1]
+
+def quickSort(externalListObject): 
+    
+    i = 0
+    internalListObject = externalListObject
+    sortedList = []
+    
+    while len(internalListObject) > i:
+        sortedList = sortedList + [min(internalListObject)]
+        internalListObject = filter(lambda x: x > min(internalListObject), internalListObject)
+    print(sortedList)
+        
+if __name__ == '__main__':
+    quickSort(integer)
