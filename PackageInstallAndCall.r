ff0860e4efcb9d44850a16f7ea31fd4f2c1d5d79
Obaid Pervaiz Gill <obaidpervaizgill@gmail.com>
2016-07-11 15:28:15 +1000

    Update PackageInstallAndCall.r

--- PackageInstallAndCall.r
+++ PackageInstallAndCall.r
@@ -1,17 +1,16 @@
-#multiply.seq
-#The function
-#@ x is a parameter for the starting number of the interval
-#@ y is a paramater for the ending number of the interval
-#@ z is a paramater for the interval
+#package.installcall
+#The function installs and calls a package if not available else just calls a package 
+#@ packages is parameter for the installation package 
 
-multiply.seq <- function(x=from,y=to,z=by){
-  a <- seq(x,y,z)
-  d <- a[1]
-  for(i in 1:length(a[-1])){
-    b <- a[i]
-    c <- a[!a %in% b]
-    rm(b)
-    d <- d*c[i]
-  }
-  return(d)
+
+package.installcall <-  function (packages = c("")){ 
+  
+  new_packages <- packages[!(packages %in% installed.packages()[,"Package"])]
+  
+  if(length(new_packages)> 0) {
+    install.packages(packages); 
+    sapply(packages, require, character.only = TRUE)
+  } else {
+    sapply(packages, require, character.only = TRUE)
+    }
 }
