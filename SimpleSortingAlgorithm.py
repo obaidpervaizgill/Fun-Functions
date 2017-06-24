dd074fba2c79d07bf320359ef94d7531b02208ac
Obaid Pervaiz Gill <obaidpervaizgill@gmail.com>
2017-06-25 01:10:25 +1000

    Update SimpleSortingAlgorithm.py

--- SimpleSortingAlgorithm.py
+++ SimpleSortingAlgorithm.py
@@ -7,12 +7,11 @@ unsorted_i = 0
 for unsorted_i in xrange(n):
     unsorted_t = int(raw_input().strip())
     unsorted.append(unsorted_t)
+
 i = 0    
 while len(unsorted) > i:
     sortedList = sortedList + filter(lambda x: x == min(unsorted), unsorted)
     unsorted = filter(lambda x: x > min(unsorted), unsorted)
-    
-    
     
 for i in sortedList:
     print i
